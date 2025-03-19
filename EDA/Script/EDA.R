29341f0a544a80c24ba311f86d6d274b37496e98
mlatinov <latinovmetodi@gmail.com>
2025-03-19 10:57:14 +0200

    # Begin the Hypothesis Testing

--- EDA/Script/EDA.R
+++ EDA/Script/EDA.R
@@ -1,7 +1,8 @@
 
 #### Libraries ####
 library(tidyverse)
-
+library(rstatix)
+library(corrplot)
 
 # Load the data 
 marine_engine_data <- read_csv("Data/marine_engine_data.csv")
@@ -49,8 +50,79 @@ marine_data %>%
     strip.text.x = element_text(size = 10),
     strip.text = element_text(size = 10)
   )
-  
-  
+
+## Correlation matrix
+dev.new(width = 10, height = 10)  
+corrplot(cor(marine_data %>% select_if(is.numeric)), 
+         method = "color", 
+         order = "hclust", 
+         tl.srt = 45, 
+         outline = TRUE, 
+         addCoef.col = "black")
+
+
+#### Hypothesis Testing
+# Sig level = 0.05 
+
+## Kruskal-Wallis Test (Non-Parametric ANOVA)
+
+# Check is there a dif between oil distribution facet by failure mode
+kruskal.test(oil_pressure ~ failure_mode,data = marine_data) # p-value = 0.2831
+ # Check the effect size 
+kruskal_effsize(data = marine_data,formula = oil_pressure ~ failure_mode) # effsize = 0.000155
+
+
+# Check is there a dif between running_period distribution facet by failure mode 
+kruskal.test(running_period ~ failure_mode,data = marine_data) # p-value = 0.8317
+# Check the effect size 
+kruskal_effsize(data = marine_data,formula = running_period ~ failure_mode) # effsize =-0.000409
+
+
+# Check if rpm distribution dif between failure modes 
+kruskal.test(rpm ~ failure_mode,data = marine_data) # p-value = 0.192
+# Check the effect size 
+kruskal_effsize(data = marine_data,formula = rpm ~ failure_mode) # effsize = 0.000334
+
+
+# Check if fuel_consumption dif between failure modes 
+kruskal.test(fuel_consumption ~ failure_mode,data = marine_data) # p-value = 0.8757
+# Check the effect size 
+kruskal_effsize(data = marine_data,formula = fuel_consumption ~ failure_mode) # effsize = -0.000445
+
+
+# Check if the exhaust_temp dif between failure modes 
+kruskal.test(exhaust_temp ~ failure_mode,data = marine_data) # p-value = 0.4094
+# Check the effect size 
+kruskal_effsize(data = marine_data,exhaust_temp ~ failure_mode) # effsize = -0.0000217
+
+
+# Check if the engine_temp is dif between failure modes
+kruskal.test(engine_temp ~ failure_mode,data = marine_data)  # p-value = 0.2566
+# Check the effect size 
+kruskal_effsize(data = marine_data,engine_temp ~ failure_mode) # effsize = 0.000201
+
+
+# Check if vibration level dist is dif between failure modes
+kruskal.test(vibration_level ~ failure_mode ,data = marine_data) # p-value = 0.2233
+# Check the effect size 
+kruskal_effsize(data = marine_data,vibration_level ~ failure_mode) # effsize =0.000266
+
+
+## Kolmogorov-Smirnov (KS) Test
+
+# Oil Pressure dif for Oil_Leakage and No Failure
+ks.test(
+  marine_data$oil_pressure[marine_data$failure_mode == "Oil Leakage"],
+  marine_data$oil_pressure[marine_data$failure_mode == "No Failure"]
+  ) # p-value = 0.3119 
+
+# 
+marine_data %>%
+  filter(failure_mode == c("No Failure","Oil Leakage"))%>%
+ggplot(aes(x = oil_pressure,color = failure_mode))+
+  stat_ecdf()
+
+
 
 
 
