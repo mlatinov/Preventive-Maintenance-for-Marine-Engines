10f69a391dcfaed2ceb244fd0ce5648e0689ac6d
mlatinov <latinovmetodi@gmail.com>
2025-03-21 21:40:18 +0200

    # Comapare Models with different recipes

--- Models/Models.R
+++ Models/Models.R
@@ -16,11 +16,13 @@ split_a <- initial_split(data = marine_data,prop = 0.8,strata = failure_mode)
 training_data_a <- training(split_a)
 testing_data_a <- testing(split_a)
 
-# Make a coltrol recipe
-recipe_control <-recipe(failure_mode ~ .,data = training_data_a)
-
-## Recipe one Standart Features
-recipe_standart <- recipe(failure_mode ~ .,data = training_data_a) %>%
+# Make a control recipe
+control_recipe <-recipe(failure_mode ~ .,data = training_data_a)%>%
+  # Dummy encode nominal predictors to prevent errors
+  step_dummy(all_nominal_predictors())
+  
+## Recipe one Standard Features
+recipe_standard <- recipe(failure_mode ~ .,data = training_data_a) %>%
   
   # Remove nzv Features
   step_nzv(all_nominal_predictors()) %>%
@@ -28,7 +30,7 @@ recipe_standart <- recipe(failure_mode ~ .,data = training_data_a) %>%
   # Apply Transformation Yeo-Jonhson
   step_YeoJohnson(all_numeric_predictors()) %>%
   
-  # Standartize the variables 
+  # Standardize the variables 
   step_center(all_numeric_predictors()) %>%
   step_scale(all_numeric_predictors()) %>%
   
@@ -87,19 +89,80 @@ tuned_svm <- svm_rbf(
   set_engine("kernlab") %>%
   set_mode("classification")
 
-## Tune the Standart Recipe 
+## Tune the Standard Recipe
 
+# Workflow_set
+workflow_recipe_tune_standard <- 
+  workflow_set(
+    preproc = list(standard = recipe_standard),
+    models = list(c_rf = control_rf, c_xgb = control_xgb, c_svm = control_svm))
 
+# Params and Grid
+resample_recipe <- vfold_cv(data = training_data_a,v = 10) 
 
-# Create a workflow set to compare diffrent recipes
+# Grid recipe Standard LHC
+tune_grid_recipe_standard <- 
+  grid_space_filling(
+    num_comp(c(1,10)),
+    predictor_prop(c(0,1)))
+
+# Tuning 
+recipe_standard_tune <- 
+  workflow_recipe_tune_standard %>%
+  workflow_map(
+    fn = "tune_grid",
+    grid = tune_grid_recipe_standard,
+    resamples = resample_recipe)
+
+# Select best params
+best_standard_params <- recipe_standard_tune %>%
+  extract_workflow_set_result("standard_c_rf")%>%
+  select_best(metric = "accuracy")
+
+# Update the recipe standard
+final_recipe_standard <- finalize_recipe(recipe_standard, best_standard_params)
+
+# Create a workflow set to compare different recipes
 workflow_recipe <- workflow_set(
-  preproc = list(simple = recipe_standart, control = control_recipe),
+  preproc = list(simple = final_recipe_standard, control = control_recipe),
   models = list(c_rf = control_rf, c_xgb = control_xgb, c_svm = control_svm),
   cross = TRUE)
 
-# Evaluate the performance for differnt recipes
+# Evaluate the performance for different recipes
 recipe_result <- workflow_recipe %>%
-  workflow_map("fit_resamples")
+  workflow_map("fit_resamples",verbose = TRUE,resamples = resample_recipe)
 
-  
-  
+# Collect the results
+all_metrics <- recipe_result %>%
+  select(wflow_id, result) %>%  # Keep workflow IDs and results
+  mutate(metrics = map(result, collect_metrics)) %>%  # Extract metrics
+  select(wflow_id, metrics) %>% 
+  unnest(metrics) 
+
+# Plot the results
+all_metrics %>%
+  filter(.metric != "brier_class") %>%
+  mutate(
+    wflow_id = fct_reorder(as.factor(wflow_id), mean, .fun = sum)  # Reorder by mean
+  ) %>%
+  ggplot(aes(x = mean, y = wflow_id, fill = .metric)) + 
+  geom_col() +
+  facet_wrap(~.metric, scales = "free_y") +
+  scale_fill_manual(values = c("accuracy" = "green", "roc_auc" = "blue")) +
+  theme_minimal() +
+  coord_cartesian(xlim = c(0.01,0.6),ylim = c(6,1),expand = TRUE)+
+  labs(
+    title = "Compare Different Models with Different Recipes ",
+    x = "Mean",
+    y = "Models",
+    subtitle = "cr = Control, simple = Standard",
+    fill = "Metric"
+  ) +
+  theme(
+    title = element_text(size = 15, face = "bold"),
+    strip.text = element_text(size = 10)
+  )
+
+
+
+
