41604c269c7676b6bb82b7287b2fadf07a5ab68f
mlatinov <latinovmetodi@gmail.com>
2025-03-22 23:05:10 +0200

    # Complete feature engineering

--- Models/Models.R
+++ Models/Models.R
@@ -2,10 +2,10 @@
 #### Libraries ####
 library(tidymodels)
 library(tidyverse)
+library(baguette)
 
 # Load the data 
 marine_data <- read.csv("marine_engine_data.csv",stringsAsFactors = TRUE)
-
 
 #### Feature Engineering ####
 
@@ -43,6 +43,39 @@ recipe_standard <- recipe(failure_mode ~ .,data = training_data_a) %>%
   # Dummy encode nominal predictors
   step_dummy(all_nominal_predictors())
 
+## Recipe two New Features
+recipe_new_features <- recipe(failure_mode ~ .,data = training_data_a)%>%
+     
+     # Step mutate adding new features
+    step_mutate(
+         temp_diff = exhaust_temp - coolant_temp,
+         fuel_efficiency = fuel_consumption / rpm,
+         vibration_ratio = vibration_level / engine_load
+         ) %>%
+  
+  #Remove original features that were used to create the new ones
+  step_rm(exhaust_temp, coolant_temp, fuel_consumption, rpm, vibration_level, engine_load) %>%
+  
+   # Remove nzv Features
+  step_nzv(all_nominal_predictors()) %>%
+     
+   # Apply Transformation Yeo-Jonhson
+  step_YeoJohnson(all_numeric_predictors()) %>%
+    
+  # Standardize the variables 
+  step_center(all_numeric_predictors()) %>%
+  step_scale(all_numeric_predictors()) %>%
+     
+     # Dim Reduction with PLS
+  step_pls(
+    all_numeric_predictors(),
+    outcome = "failure_mode",
+    num_comp = tune(),
+    predictor_prop = tune()) %>%
+    
+  # Dummy encode nominal predictors
+  step_dummy(all_nominal_predictors())
+  
 
 ##### Define models ####
 
@@ -89,13 +122,29 @@ tuned_svm <- svm_rbf(
   set_engine("kernlab") %>%
   set_mode("classification")
 
+# Single Layer Neural Network Model
+slnn <- mlp() %>%
+  set_engine("nnet")%>%
+  set_mode("classification")
+
+# Bagged Neural Network Model Specification
+bnn <- bag_mlp()%>%
+  set_engine("nnet")%>%
+  set_mode("classification")
+
+# Null Model 
+null_model <- null_model()%>%
+  set_engine("parsnip")%>%
+  set_mode("classification")
+
 ## Tune the Standard Recipe
 
 # Workflow_set
 workflow_recipe_tune_standard <- 
   workflow_set(
     preproc = list(standard = recipe_standard),
-    models = list(c_rf = control_rf, c_xgb = control_xgb, c_svm = control_svm))
+    models = list(c_rf = control_rf, c_xgb = control_xgb, c_svm = control_svm,c_slnn = slnn,bnn = bnn,null_model=null_model))
+
 
 # Params and Grid
 resample_recipe <- vfold_cv(data = training_data_a,v = 10) 
@@ -122,9 +171,39 @@ best_standard_params <- recipe_standard_tune %>%
 # Update the recipe standard
 final_recipe_standard <- finalize_recipe(recipe_standard, best_standard_params)
 
+# Create a workflow_set for new features recipe 
+workflow_recipe_tune_newfeatures <-
+  workflow_set(
+    preproc = list(recipe = recipe_new_features),
+    models = list(random_forest = control_rf,svm = control_svm,xgb = control_xgb))
+
+# Tune grid new features recipe
+tune_grid_recipe_new_features <-
+  grid_space_filling(
+    num_comp(c(1,10)),
+    predictor_prop(c(0,1)))
+
+# Tune new features recipe
+recipe_new_features_tune <-
+  workflow_recipe_tune_newfeatures %>%
+  workflow_map(
+    fn = "tune_grid",
+    resamples = resample_recipe,
+    grid = tune_grid_recipe_new_features,
+    verbose = TRUE)
+
+# Select Best params
+best_new_features_params <-
+  recipe_new_features_tune %>%
+  extract_workflow_set_result("recipe_svm","recipe_random_forest","recipe_xgb")%>%
+  select_best(metric = "accuracy")
+
+# Update 
+finalize_new_feature_recipe <- finalize_recipe(recipe_new_features,best_new_features_params)
+
 # Create a workflow set to compare different recipes
 workflow_recipe <- workflow_set(
-  preproc = list(simple = final_recipe_standard, control = control_recipe),
+  preproc = list(simple = final_recipe_standard, control = control_recipe,new_f_recipe = finalize_new_feature_recipe),
   models = list(c_rf = control_rf, c_xgb = control_xgb, c_svm = control_svm),
   cross = TRUE)
 
@@ -150,7 +229,7 @@ all_metrics %>%
   facet_wrap(~.metric, scales = "free_y") +
   scale_fill_manual(values = c("accuracy" = "green", "roc_auc" = "blue")) +
   theme_minimal() +
-  coord_cartesian(xlim = c(0.01,0.6),ylim = c(6,1),expand = TRUE)+
+  coord_cartesian(xlim = c(0.01,0.6),ylim = c(9,1),expand = TRUE)+
   labs(
     title = "Compare Different Models with Different Recipes ",
     x = "Mean",
